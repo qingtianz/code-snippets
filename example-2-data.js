dd2c02b408fa9e0cdc050dbc9dbe074fc84f6127
Qingtian Zhou <6309828+qingtianz@users.noreply.github.com>
2019-02-17 19:58:40 -0600

    Clean commit

---
+++ example-2-data.js
@@ -0,0 +1,10 @@
+var ctxData = {
+    datasets: [{
+        label: "Frequency count",
+        backgroundColor: "rgba(11, 57, 84, 0.2)",
+        hoverBackgroundColor: "rgba(11, 57, 84, 0.5)",
+        borderColor: "rgba(11, 57, 84, 1)",
+        borderWidth: 1,
+        data: [{"x":"Disabled","y":"Divorced","r":9.899495},{"x":"Disabled","y":"Married","r":15.96872},{"x":"Disabled","y":"Single","r":7.211103},{"x":"Employed","y":"Divorced","r":29.83287},{"x":"Employed","y":"Married","r":62.18521},{"x":"Employed","y":"Single","r":30.67572},{"x":"Medical Leave","y":"Divorced","r":8.246211},{"x":"Medical Leave","y":"Married","r":16.8523},{"x":"Medical Leave","y":"Single","r":8.944272},{"x":"Retired","y":"Divorced","r":10.34408},{"x":"Retired","y":"Married","r":10.77033},{"x":"Retired","y":"Single","r":7.681146},{"x":"Unemployed","y":"Divorced","r":14.3527},{"x":"Unemployed","y":"Married","r":27.85678},{"x":"Unemployed","y":"Single","r":36.53765},],
+    }]
+};
